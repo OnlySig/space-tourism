82cc0a6bdc7129d657a8fe2566b50aa10a9ef47f
Gabriel Castro <72272327+OnlySig@users.noreply.github.com>
2023-10-26 13:44:30 -0300

    Update mobile-navbar.js

--- assets/js/mobile-navbar.js
+++ assets/js/mobile-navbar.js
@@ -3,5 +3,5 @@ const navBar = document.querySelector(".navList")
 
 btnAbrir.addEventListener("click", _ => {
     navBar.classList.toggle("active")
-    navBar.classList[1] === "active" ? btnAbrir.src = "/assets/shared/icon-close.svg" : btnAbrir.src = "/assets/shared/icon-hamburger.svg"
+    navBar.classList[1] === "active" ? btnAbrir.src = "assets/shared/icon-close.svg" : btnAbrir.src = "assets/shared/icon-hamburger.svg"
 })
